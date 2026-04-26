17fbad559d9e6b223dc50a6911b09e776485cb4e
Mahid Zulfiqar <khanmahid97@gmail.com>
2026-04-27 01:02:57 +0500

    Final Changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5050;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-     "https://navttc-assignment-projects-a43j9xvk9-mahids-projects-5669cd1b.vercel.app"
+    "https://react-product-management-website-9j83ft9fu.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
